e47587a117cce260b0794038bf45cff2103c2094
andmar1x <andmar1x@instinctools.ru>
2015-05-04 19:44:38 +0300

    Android Tests for GreenDao

---
+++ app/src/androidTest/java/org/andmar1x/androidormtests/GreenDaoTest.java
@@ -0,0 +1,84 @@
+package org.andmar1x.androidormtests;
+
+import android.database.sqlite.SQLiteDatabase;
+
+import org.andmar1x.androidormtests.greendao.DaoMaster;
+import org.andmar1x.androidormtests.greendao.DaoSession;
+import org.andmar1x.androidormtests.greendao.Entry;
+import org.andmar1x.androidormtests.greendao.EntryDao;
+import org.andmar1x.androidormtests.greendao.EntryDb;
+import org.andmar1x.androidormtests.test.DatabaseTestCase;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by andmar1x on 5/4/15.
+ */
+public class GreenDaoTest extends DatabaseTestCase {
+
+    private DaoSession mDaoSession;
+    private EntryDao mEntryDao;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        SQLiteDatabase db = new DaoMaster.DevOpenHelper(mContext, EntryDb.NAME, null).getWritableDatabase();
+        mDaoSession = new DaoMaster(db).newSession();
+        mEntryDao = mDaoSession.getEntryDao();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+
+        mEntryDao.deleteAll();
+        mDaoSession.getDatabase().close();
+    }
+
+    @Override
+    public void testSingleInsert() {
+        insert(false);
+    }
+
+    @Override
+    public void testBulkInsert() {
+        insert(true);
+    }
+
+    private void insert(boolean isBulk) {
+        if (isBulk) {
+            mDaoSession.runInTx(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 1; i <= Consts.ITEMS_COUNT; ++i) {
+                        insertOne(i);
+                    }
+                }
+            });
+        } else {
+            for (int i = 1; i <= Consts.ITEMS_COUNT; ++i) {
+                insertOne(i);
+            }
+        }
+
+        List<Entry> results = mEntryDao.queryBuilder().where(EntryDao.Properties.BooleanValue.eq("TRUE")).list();
+
+        assertEquals(Consts.ITEMS_COUNT / 2, results.size());
+    }
+
+    private void insertOne(int i) {
+        Entry entry = new Entry();
+        entry.setBooleanValue(i % 2 == 0);
+        entry.setShortValue((short) i);
+        entry.setIntValue(i);
+        entry.setLongValue((long) i);
+        entry.setFloatValue((float) i);
+        entry.setDoubleValue((double) i);
+        entry.setStringValue(Entry.class.getSimpleName() + " " + i);
+        entry.setDateValue(new Date());
+
+        mEntryDao.insert(entry);
+    }
+}
