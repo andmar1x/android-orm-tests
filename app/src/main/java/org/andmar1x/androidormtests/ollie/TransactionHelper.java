8ab7bf0d1d05ecece80705ad29c57f23d9ceffb1
andmar1x <andmar1x@instinctools.ru>
2015-05-15 17:28:02 +0300

    TransactionHelper for Ollie

---
+++ app/src/main/java/org/andmar1x/androidormtests/ollie/TransactionHelper.java
@@ -0,0 +1,33 @@
+package org.andmar1x.androidormtests.ollie;
+
+import android.database.sqlite.SQLiteDatabase;
+
+import ollie.Ollie;
+
+/**
+ * Created by andmar1x on 5/15/15.
+ */
+public class TransactionHelper {
+
+    public static void doInTransaction(Callback callback) {
+        synchronized (Ollie.class) {
+            SQLiteDatabase db = Ollie.getDatabase();
+            db.beginTransaction();
+            try {
+                callback.onProcess();
+                db.setTransactionSuccessful();
+            } catch (Exception e) {
+                callback.onFail();
+            } finally {
+                db.endTransaction();
+            }
+            callback.onResultReceived();
+        }
+    }
+
+    public interface Callback {
+        void onProcess();
+        void onFail();
+        void onResultReceived();
+    }
+}
